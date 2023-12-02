fbd07d5df19a1b21871e1f5c0c8c47bcbcf7f711
Suyane Santos <132683758+suyane924@users.noreply.github.com>
2023-12-02 19:34:08 -0300

    Update CorpoHome.jsx

--- OdontoVidaPrj-main/OdontoVidaPrj-main/ProjetoOdonto/vite/src/components/CorpoHome.jsx
+++ OdontoVidaPrj-main/OdontoVidaPrj-main/ProjetoOdonto/vite/src/components/CorpoHome.jsx
@@ -4,8 +4,7 @@ const CorpoHome = () => {
             <div className="containerHome">
                 <div className="BlocoHomeEsq">
                 <h1 className="TitleHome">Seja Bem Vindo!</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque pariatur ea quae numquam odio aliquid aspernatur reiciendis nulla minima quod quibusdam suscipit quasi magni molestiae quis iste, rerum officiis veniam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sunt harum labore eaque distinctio, repellat iste voluptas, rem nostrum eos aspernatur qui excepturi officia magnam vel. Id alias illo commodi?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facere eveniet modi numquam, cupiditate ipsam doloremque quam exercitationem maxime adipisci quaerat temporibus tenetur delectus, quae quibusdam hic, deserunt accusantium officiis. </p>
+                <p>Juntos, construímos sorrisos e bem-estar.</p>
                 
                 </div>
                 <div className="BlocoHomeDir">
