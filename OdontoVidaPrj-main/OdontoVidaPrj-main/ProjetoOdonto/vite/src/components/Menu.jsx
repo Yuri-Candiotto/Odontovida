c5f4da81801e114f23319d0f702cb992775180ac
Suyane Santos <132683758+suyane924@users.noreply.github.com>
2023-12-03 20:45:50 -0300

    Update Menu.jsx

--- OdontoVidaPrj-main/OdontoVidaPrj-main/ProjetoOdonto/vite/src/components/Menu.jsx
+++ OdontoVidaPrj-main/OdontoVidaPrj-main/ProjetoOdonto/vite/src/components/Menu.jsx
@@ -7,11 +7,12 @@ const Menu = () => {
             <div className='containerMenu'>
         <Link to="../TelaHome"><div className='childMenu'> <button className="styleMenu">Home</button>      </div></Link>
 
-            <Link to="../TelaCadastro"><div className='childMenu'><button className="styleMenu">Cadastro</button></div> </Link>            
+            <Link to="../TelaCadastro"><div className='childMenu'><button className="styleMenu">Cadastro</button></div> </Link>
+                
+            <Link to="../TelaServicos"><div className='childMenu'><button className="styleMenu">Serviços</button></div> </Link> 
                
                 
                 <div className='childMenu'><button className="styleMenu">Agendamento</button></div>
-                <div className="childMenu"><button className="styleMenu">Serviços</button></div>
                 <div className="childMenu"><button className="styleMenu">Relatórios</button></div>
             </div>
         </div>
