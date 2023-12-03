10502be15e964c25c3116ee8cd995744521d0570
Suyane Santos <132683758+suyane924@users.noreply.github.com>
2023-12-03 20:20:31 -0300

    Add files via upload

---
+++ OdontoVidaPrj-main/OdontoVidaPrj-main/ProjetoOdonto/vite/src/pages/TelaServicos.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import SuperiorCadastroServic from "../components/SuperiorCadastroServic"
+
+const TelaServicos = () => {
+    return(
+        <div>
+            <div className="headerPosition"><Header /></div>
+            <SuperiorCadastroServic/>
+        </div>
+    )
+}
+
+export default TelaServicos
